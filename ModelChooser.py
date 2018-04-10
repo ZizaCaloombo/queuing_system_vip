09af2cab7815aac223a1be0ba1046cbdff28a515
ZizaCaloombo <erep41@mail.ru>
2018-04-11 02:06:43 +0300

    Not working again

--- ModelChooser.py
+++ ModelChooser.py
@@ -22,7 +22,7 @@ def print_results(avg_times, rez_time, srv_mu):
     plt.savefig('results.pdf')
 
 
-def choose_model(stream_num, avg_times, lambdas, mu, query_num, test_num, comp_accur, img_tracing=True):
+def choose_model(stream_num, avg_times, lambdas, mu, query_num, test_num, comp_accur, p_val):
     t_proc = 1 / mu  # Request processing time
     print('Начальные значения mu = ' + str(mu) + " (t_proc = " + str(t_proc) + ")")
 
@@ -34,7 +34,7 @@ def choose_model(stream_num, avg_times, lambdas, mu, query_num, test_num, comp_a
 
 
     # p_val = np.concatenate([np.arange(0.1, 0.4, 0.1), np.arange(0.4, 0.61, 0.001), np.arange(0.7, 1, 0.1)])
-    p_val = np.arange(0, 1.1, 0.1)  # Распределение вероятностей для соблюдения среднего времени выполнения заявок
+
     # p_val = np.array([0, 0.3, 0.5, 0.8, 1])    # Распределение вероятностей для соблюдения среднего времени выполнения заявок
     # p_val = np.arange(0.2,0.9,0.1)
     # p_val = np.array([0, 1])
@@ -43,7 +43,7 @@ def choose_model(stream_num, avg_times, lambdas, mu, query_num, test_num, comp_a
     rez_time = []
     srv_mu = []
     algo_revers = False
-    cur_accur = 1e0
+    cur_accur = 1e2
     while True:
         rand_arr = []
         for i in range(test_num):
@@ -58,18 +58,18 @@ def choose_model(stream_num, avg_times, lambdas, mu, query_num, test_num, comp_a
                 rez = se.run_execution(p, copy.deepcopy(rand_arr[k]))
                 for i in range(stream_num):
                     avg_est[i, val] += rez[i]
-            print('p=' + str(p_val[val]) + ' successfully processed')
+            #print('p=' + str(p_val[val]) + ' successfully processed')
         avg_est /= test_num
         if not algo_revers:
             rez_time.append(avg_est)
             srv_mu.append(1 / t_proc)
 
-        print('avg')
-        avg1 = avg_est[0, :].tolist().index(min(avg_est[0, :]))
-        avg2 = avg_est[1, :].tolist().index(min(avg_est[1, :]))
-        print([avg_est[0, avg1], avg_est[1, avg1]])
-        print([avg_est[0, avg2], avg_est[1, avg2]])
-        print('t_proc = ', t_proc, ' mu=', 1 / t_proc)
+        # print('avg')
+        # avg1 = avg_est[0, :].tolist().index(min(avg_est[0, :]))
+        # avg2 = avg_est[1, :].tolist().index(min(avg_est[1, :]))
+        # print([avg_est[0, avg1], avg_est[1, avg1]])
+        # print([avg_est[0, avg2], avg_est[1, avg2]])
+        # print('t_proc = ', t_proc, ' mu=', 1 / t_proc)
 
         # Обратный проход с целью максимально приблизится к требуемым значениям среднего времени
         if algo_revers:
@@ -78,9 +78,10 @@ def choose_model(stream_num, avg_times, lambdas, mu, query_num, test_num, comp_a
                 if avg_est[0, i] < avg_times[0] and avg_est[1, i] < avg_times[1]:
                     flag = True
                     opt_val = i
-                    print('\navg_rez')
-                    print(avg_est[:, i])
-                    print('p=' + str(p_val[i]))
+
+                    # print('\navg_rez')
+                    # print(avg_est[:, i])
+                    # print('p=' + str(p_val[i]))
                     break
             if flag:  # Если значение критерия всё ещё есть
                 # Нужно увеличивать t_proc или уменьшать mu
@@ -104,9 +105,9 @@ def choose_model(stream_num, avg_times, lambdas, mu, query_num, test_num, comp_a
             for i in range(len(p_val)):
                 if avg_est[0, i] < avg_times[0] and avg_est[1, i] < avg_times[1]:
                     opt_val = i
-                    print('\navg_rez')
-                    print(avg_est[:, i])
-                    print('p=' + str(p_val[i]))
+                    # print('\navg_rez')
+                    # print(avg_est[:, i])
+                    # print('p=' + str(p_val[i]))
                     print('===Reverse time===')
                     algo_revers = True
                     temp_est = avg_est
@@ -121,12 +122,15 @@ def choose_model(stream_num, avg_times, lambdas, mu, query_num, test_num, comp_a
                 if t_proc < 1e-5:
                     print('Заданные требования достижимы лишь при малых значениях alpha')
                     break
-    print_results(avg_times, rez_time, srv_mu)
+    # print_results(avg_times, rez_time, srv_mu)
+    # avg_times - tao1,2 (needed times)
+    # rez_time
+
     print('rez')
     print(np.mean(avg_est[:, opt_val]))
     print(1 / (srv_mu[-1] - sum(lambdas)))
     print('p = ', p_val[opt_val])
-    return
+    return rez_time, srv_mu
 
 
 if __name__ == '__main__':
@@ -141,7 +145,16 @@ if __name__ == '__main__':
     query_num = 10000  # Общее количество заявок, которые поступят в каждый буфер. т.е. q_n в 1 и во 2.
     comp_accur = 1e-1  # Точность вычислений при обратном проходе (кол-во знаков после запятой)
     test_num = 10  # Количество тестов с разными рандомами (сглаживает графики?)
-    img_tracing = True
+    prob = np.arange(0, 1.1, 0.1)  # Распределение вероятностей для соблюдения среднего времени выполнения заявок
 
-    choose_model(stream_num, avg_times, lambdas, mu, query_num, test_num, comp_accur, img_tracing)
+    x, y = choose_model(stream_num, avg_times, lambdas, mu, query_num, test_num, comp_accur, prob)
 
+    x1 = x[-1]
+    y1 = y[-1]
+    val_num = len(x1[-1])
+    opt = [0] * val_num
+    for i in range(val_num):
+        opt[i] = abs(x1[0][i] - avg_times[0]) + abs(x1[1][i] - avg_times[1])
+    print('Final!!!Opt')
+    print(prob[np.argmin(opt)])
+    print(np.min(opt))
--- ProbDependenceExp.py
+++ ProbDependenceExp.py
@@ -1,5 +1,57 @@
-mu = 1
+import numpy as np
+import matplotlib.pyplot as plt
+
+import ModelChooser as MCh
+
+
+stream_num = 2      # Количество потоков
+# Время в 2 раза больше
+# Интенсивность в 2 раза меньше
+avg_times = [1, 1]  # Среднее время для каждой категории заявок
+# mu = 0.2
+# avg_times = [2, 1]  # Среднее время для каждой категории заявок
+# mu = 0.1
+lambdas = [0.5, 0.5]  # Интенсивности поступления заявок в каждый буфер
+query_num = 10000  # Общее количество заявок, которые поступят в каждый буфер. т.е. q_n в 1 и во 2.
+comp_accur = 1e-1  # Точность вычислений при обратном проходе (кол-во знаков после запятой)
+test_num = 10  # Количество тестов с разными рандомами (сглаживает графики?)
+prob = np.arange(0, 1.1, 0.1)  # Распределение вероятностей для соблюдения среднего времени выполнения заявок
+
 tau = [[4, 1], [2, 1], [1, 1], [1, 2], [1, 4]]
 
-for i in range(len(tau)):
-    pass
+opt = [0] * len(tau)
+
+multi_num = 3
+rez = [0] * len(tau)
+full_rez = [rez] * 3
+mu = 1
+for f in range(multi_num):
+    for i in range(len(tau)):
+        if f > 0:
+            tau[i][0] /= 2
+            tau[i][1] /= 2
+            mu *= 2
+        avg_times = tau[i]
+
+        x, y = MCh.choose_model(stream_num, avg_times, lambdas, mu, query_num, test_num, comp_accur, prob)
+
+        x1 = x[-1]
+        y1 = y[-1]
+        val_num = len(x1[-1])
+        opt = [0] * val_num
+        for k in range(val_num):
+            opt[i] = abs(x1[0][k] - avg_times[0]) + abs(x1[1][k] - avg_times[1])
+        full_rez[f][i] = prob[np.argmin(opt)]
+        print('Выполнено:', str(i+1), '/', str(len(tau)))
+    print('=========')
+    print('Всего:', str(f + 1), '/', str(multi_num))
+
+plt.figure(1)
+plt.xlabel('tau1/tau2')
+plt.ylabel('p_opt')
+for i in range(multi_num):
+    plt.plot(range(1, 5), full_rez[i], 'x', label="Mu = " + "{0:.2f}".format(mu))
+plt.legend(loc='best')
+plt.grid()
+# plt.show()
+plt.savefig('ProbTest.pdf')
